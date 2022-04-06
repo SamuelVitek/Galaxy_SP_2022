61c52e9731f590c9b1645349282299a63cbdea89
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-04-06 20:22:19 +0200

    Pokusné kolečko po posunutí souřadnic na střed okna

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -8,6 +10,17 @@ public class DrawingPanel extends JPanel {
 		this.setPreferredSize(new Dimension(800, 600));
 	}
 
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
 
+		Graphics2D g2 = (Graphics2D)g;
+
+		g2.translate(getWidth()/2, getHeight()/2);
+
+		Ellipse2D tryf = new Ellipse2D.Double(0,0,10,10);
+		g2.setColor(Color.BLUE);
+		g2.fill(tryf);
+	}
 
 }
