9564a06ca75969ffb9aae0694e745ae54bd401c1
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-04-06 19:37:10 +0200

    Přidání okna

---
+++ src/DrawingPanel.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class DrawingPanel extends JPanel {
+	public DrawingPanel() {
+		this.setPreferredSize(new Dimension(800, 600));
+	}
+
+
+
+}
--- src/Galaxy_SP2022.java
+++ src/Galaxy_SP2022.java
@@ -1,8 +1,21 @@
+import javax.swing.*;
+
 public class Galaxy_SP2022 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
+		JFrame okno = new JFrame();
+		okno.setTitle("Galaxy Seminární Práce - Samuel Vítek, A21B0315P");
+		okno.setSize(640, 480);
+
+		DrawingPanel panel = new DrawingPanel();
+		okno.add(panel);
+
+		okno.pack();
+
+		okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		okno.setLocationRelativeTo(null);
+		okno.setVisible(true);
 
 		LoadData ld = new LoadData("pulsar.csv");
 	}
