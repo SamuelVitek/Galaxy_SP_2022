a2e92836c2ca353b74d5b06779f46c31394c84e9
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-04-10 23:07:50 +0200

    Přidání scale a pozicování

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -15,20 +15,40 @@ public class DrawingPanel extends JPanel {
 
 		Graphics2D g2 = (Graphics2D)g;
 
-		g2.translate(getWidth()/2, getHeight()/2);
-
-		LoadData ld = new LoadData("negative.csv");
+		LoadData ld = new LoadData("random500.csv");
 		List<Planet> planets = ld.getPlanets();
 
-		double scale;
+		double spaceWidth = Math.abs(ld.getSpaceEndX()) + Math.abs(ld.getSpaceStartX());
+		double spaceHeight = Math.abs(ld.getSpaceEndY()) + Math.abs(ld.getSpaceStartY());
+
+		double scaleX = this.getWidth() / spaceWidth;
+		double scaleY = this.getHeight() / spaceHeight;
+		double scale = Math.min(scaleX, scaleY);
+
 		Ellipse2D planetDraw;
 
 		for (Planet planet : planets) {
-			planetDraw = new Ellipse2D.Double(planet.getxPosition(), planet.getyPosition(), planet.getR(), planet.getR());
+
+			double scaledR = planet.getR() * scale;
+
+			double scaledPosX = (planet.getxPosition() * scale) - scaledR / 2;
+			double scaledPosY = (planet.getyPosition() * scale) - scaledR / 2;
+
+			double startX = (this.getWidth()  / 2.0) + scaledPosX;
+			double startY = (this.getHeight() / 2.0) + scaledPosY;
+
+			planetDraw = new Ellipse2D.Double(
+					startX,
+					startY,
+					scaledR,
+					scaledR);
 
 			g2.setColor(Color.BLUE);
 			g2.fill(planetDraw);
 		}
+
+
+
 	}
 
 }
