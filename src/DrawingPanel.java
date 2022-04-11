088bc6cc1891b9c7a76500b84578b578117fdadc
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-04-11 13:48:45 +0200

    Výpočet n-objektů

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.swing.JPanel;
 
 public class DrawingPanel extends JPanel {
-	//private double time;
+	private double time;
+	private final LoadData ld = new LoadData("collision.csv");
+	private final List<Planet> planets = ld.getPlanets();
 
 	public DrawingPanel() {
 		this.setPreferredSize(new Dimension(800, 600));
@@ -16,9 +18,6 @@ public class DrawingPanel extends JPanel {
 		super.paint(g);
 
 		Graphics2D g2 = (Graphics2D)g;
-
-		LoadData ld = new LoadData("collision.csv");
-		List<Planet> planets = ld.getPlanets();
 
 		double spaceWidth = ld.getSpaceEndX() - ld.getSpaceStartX();
 		double spaceHeight = ld.getSpaceEndY() - ld.getSpaceStartY();
@@ -53,12 +52,70 @@ public class DrawingPanel extends JPanel {
 		}
 	}
 
-//	public void setTime(double time) {
-//		this.time = time;
-//	}
-//
-//	public double getTime() {
-//		return time;
-//	}
+	public void update(double t) {
+		double dt_min = ld.getTimeStep() / 100;
 
+		while (t > 0) {
+			double dt = Math.min(t, dt_min);
+
+			for (Planet planet : planets) {
+				double aix = computeAcceleration(planet)[0];
+				double aiy = computeAcceleration(planet)[1];
+
+				//x-ove souradnice rychlosti a pozic a jejich vypocty
+				double vix = planet.getxSpeed();
+				double pix = planet.getxPosition();
+
+				vix += 0.5 * dt * aix;
+				pix += dt * vix;
+				vix += 0.5 * dt * aix;
+
+				planet.setxSpeed(vix);
+				planet.setxPosition(pix);
+
+				//y-ove souradnice rychlosti a pozic
+				double viy = planet.getySpeed();
+				double piy = planet.getyPosition();
+
+				viy += 0.5 * dt * aiy;
+				piy += dt * viy;
+				viy += 0.5 * dt * aiy;
+
+				planet.setySpeed(viy);
+				planet.setyPosition(piy);
+			}
+
+			t = t - dt;
+		}
+	}
+
+	public double[] computeAcceleration(Planet i) {
+		List<Planet> newPlanets = planets;
+		newPlanets.remove(i);
+
+		double accelerationX = 0;
+		double accelerationY = 0;
+
+		for (Planet j : newPlanets) {
+			double px = j.getxPosition() - i.getxPosition();
+			double py = j.getyPosition() - i.getyPosition();
+			double denominator = Math.sqrt((px * px) + (py * py));
+			accelerationX += ld.getG() * j.getWeight()	* (px / Math.pow(denominator, 3.0));
+			accelerationY += ld.getG() * j.getWeight()	* (py / Math.pow(denominator, 3.0));
+		}
+
+		return new double[]{accelerationX, accelerationY};
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public double getTimePeriod() {
+		return 1000 / ld.getTimeStep();
+	}
 }
