84866663698e296115b6b03f679827adca48af76
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2022-04-12 12:48:52 +0200

    Komentáře

--- src/Planet.java
+++ src/Planet.java
@@ -1,13 +1,25 @@
+/**
+ * Třída držící hlavní informace o planetě
+ *
+ * @author Samuel Vítek
+ */
 public class Planet {
+    /** Název planety */
     private final String name;
+    /** Typ planety */
     private final String type;
+    /** Pozice planety */
     private double xPosition, yPosition;
+    /** Rychlost planety */
     private double xSpeed, ySpeed;
+    /** Hmotnost planety */
     private final double weight;
+    /** Poloměr planety */
     private final double r;
 
     /**
      * Tridni konstruktor definujici planetu
+     *
      * @param name nazev planety
      * @param type typ planety
      * @param xPosition pozice planety na ose x
@@ -27,60 +39,119 @@ public class Planet {
         this.r = r();
     }
 
+    /**
+     * Vrátí název planety
+     *
+     * @return název planety
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Vrátí typ planety
+     *
+     * @return typ planety
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Vrátí x pozici planety
+     *
+     * @return x-ova souradnice
+     */
     public double getxPosition() {
         return xPosition;
     }
 
+    /**
+     * Vrátí y pozici planety
+     *
+     * @return y-ova souradnice
+     */
     public double getyPosition() {
         return yPosition;
     }
 
+    /**
+     * Vrátí x pozici rychlosti planety
+     *
+     * @return x-ova souradnice
+     */
     public double getxSpeed() {
         return xSpeed;
     }
 
+    /**
+     * Vrátí y pozici rychlosti planety
+     *
+     * @return y-ova souradnice
+     */
     public double getySpeed() {
         return ySpeed;
     }
 
+    /**
+     * Vrátí hmotnost planety
+     *
+     * @return hmotnost planety
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Vypočte a vrátí poloměr planety
+     *
+     * @return poloměr planety
+     */
     public double r() {
-        double r = Math.cbrt(weight / (4.0/3.0) * Math.PI);
-
-        if (r > 1E6) {
-            return 1E11;
-        } else {
-            return r;
-        }
+        return Math.cbrt(weight / (4.0/3.0) * Math.PI);
     }
 
+    /**
+     * Vrátí poloměr planety
+     *
+     * @return poloměr planety
+     */
     public double getR() {
         return r;
     }
 
+    /**
+     * Nastaví pozici x planety
+     *
+     * @param xPosition x pozice
+     */
     public void setxPosition(double xPosition) {
         this.xPosition = xPosition;
     }
 
+    /**
+     * Nastaví pozici y planety
+     *
+     * @param yPosition y pozice
+     */
     public void setyPosition(double yPosition) {
         this.yPosition = yPosition;
     }
 
+    /**
+     * Nastaví rychlost x planety
+     *
+     * @param xSpeed x rychlost
+     */
     public void setxSpeed(double xSpeed) {
         this.xSpeed = xSpeed;
     }
 
+    /**
+     * Nastaví rychlost y planety
+     *
+     * @param ySpeed y rychlost
+     */
     public void setySpeed(double ySpeed) {
         this.ySpeed = ySpeed;
     }
